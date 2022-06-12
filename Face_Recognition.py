9bed218d0eda3884163535031a24b22ef7d2d399
IuLax790 <66007024+IuLax790@users.noreply.github.com>
2022-06-12 14:10:28 +0300

    Initial commit

---
+++ Face_Recognition.py
@@ -0,0 +1,16 @@
+import cv
+import cv2 as cv
+#loading the image
+img = cv.imread("C:\\Users\\idoch\\Downloads\\1653322268_158_Zlatan-Ibrahimovic-turns-a-TABLE-as-he-leads-the-wild.jpg")
+#loading haarcascade face file, helps recognise faces in an image.
+haar_cascade = cv.CascadeClassifier("C:\\Users\\idoch\\OneDrive - Bar-Ilan University\\Documents\\GitHub\\opencv\\data\\haarcascades\\haarcascade_frontalface_alt2.xml")
+#defining the scale of the faces rect
+faces_rect = haar_cascade.detectMultiScale(img,scaleFactor=1.1,minNeighbors=1)
+#show how many faces were detected
+print(f'Number of faces found={len(faces_rect)}')
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y),(x+w,y+h),(0,255,0),thickness=3)
+cv.imshow('Detected Faces',img)
+
+cv.waitKey(0)
